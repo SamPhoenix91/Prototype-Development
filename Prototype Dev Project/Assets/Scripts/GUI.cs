76c345bec78fe2aca7ef32f9f0278c4343f18ddd
Kane Garrett <kaneg79@users.noreply.github.com>
2019-03-28 23:11:28 +0000

    temp

---
+++ Prototype Dev Project/Assets/Scripts/GUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI : MonoBehaviour {
+    public static object skin { get; internal set; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Prototype Dev Project/Assets/Scripts/GUI_Menu.cs
+++ Prototype Dev Project/Assets/Scripts/GUI_Menu.cs
@@ -3,35 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GUI_Menu : MonoBehaviour {
+public class GUI_Menu : MonoBehaviour
+{
 
-    private int buttonWidth = 200;
-    private int buttonHeight = 50;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    void OnGUI()
+    
+    public void NewGame()
     {
+        SceneManager.LoadScene("Level_1");
+    }
 
-        if (GUI.Button(new Rect(1495, 800, buttonWidth, buttonHeight), "NewGame"))
-        {
-            SceneManager.LoadScene(2);
-        }
-        if (GUI.Button(new Rect(845, 800, buttonWidth, buttonHeight), "Options"))
-        {
-            SceneManager.LoadScene(3);
-        }
-        if (GUI.Button(new Rect(230, 800, buttonWidth, buttonHeight), "Quit"))
-        {
-            Application.Quit();
-        }
-        //GUI.EndGroup();
+    public void Options()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+    public void WantsToQuit()
+    {
+        Debug.Log("Has Quit the game");
+        Application.Quit();
     }
 }    
